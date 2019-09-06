9c5e6aae6fb6c6248122591fa92e40ba63d40519
Vivek Kumar Mishra <vivekmishra22117@gmail.com>
2019-09-06 16:11:57 +0530

    Update SumOfNumbersInString.java

--- src/main/java/com/doubledigit/algorithim/chapter1/algorithimChapterOne/SumOfNumbersInString.java
+++ src/main/java/com/doubledigit/algorithim/chapter1/algorithimChapterOne/SumOfNumbersInString.java
@@ -6,8 +6,10 @@ package com.doubledigit.algorithim.chapter1.algorithimChapterOne;
 public class SumOfNumbersInString {
 
     private static String inputData = "sdfse567idsi23iisodfif23";
+    static int[] inputElements = {4, 3, 2, 4, 2, 2, 4, 1, 1, 1, 1, 1, 3, 3, 0, 0, 0, 0, 3, 3, 3, 4, 4, 4};
 
     public static void main(String[] args) {
+        getIndexOfLowestOccurrencesInArrayElements(inputElements);
         int N = inputData.length();
         int sum = 0;
         int count = 0;
@@ -29,5 +31,44 @@ public class SumOfNumbersInString {
             sum = sum + Integer.parseInt(inputData.substring(N - count, N));
         }
         System.out.println("Sum of numbers in the string is: " + sum);
+    }
+    
+    private static void getIndexOfLowestOccurrencesInArrayElements(int[] inputElements) {
+        sort(inputElements);
+        int[][] output = new int[2][3];
+        int i = 0;
+        while (i < inputElements.length) {
+            int j = inputElements[i];
+            int valueCount = 1;
+            i = i + 1;
+
+            if (i > inputElements.length - 1) break;
+
+            while (inputElements[i] == j) {
+                valueCount = valueCount + 1;
+                i = i + 1;
+                if (i > inputElements.length - 1) break;
+            }
+            if (output[0][2] == 0) {
+                output[0][0] = i - 1;
+                output[0][1] = inputElements[i - 1];
+                output[0][2] = valueCount;
+            } else if (output[1][2] == 0) {
+                output[1][0] = i - 1;
+                output[1][1] = inputElements[i - 1];
+                output[1][2] = valueCount;
+            } else if (output[0][2] >= valueCount && output[0][2] > output[1][2]) {
+                output[0][0] = i - 1;
+                output[0][1] = inputElements[i - 1];
+                output[0][2] = valueCount;
+            } else if (output[1][2] >= valueCount) {
+                output[1][0] = i - 1;
+                output[1][1] = inputElements[i - 1];
+                output[1][2] = valueCount;
+            }
+        }
+        System.out.println("Element is " + output[0][1] + " and Frequency is " + output[0][2] + "\n"
+                + "Element is " + output[1][1] + " and Frequency is " + output[1][2]);
+
     }
 }
