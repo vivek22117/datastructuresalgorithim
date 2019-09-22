7201ac81677025643ef423f42f82f233eccb8d2c
vivek22117 <vivekmishra@22117>
2019-09-22 12:47:39 +0530

    Adding example to get max profit from prices available in array

---
+++ src/main/java/com/doubledigit/algorithim/chapter1/datastructures/examples/MaxProfitFromGivenPrices.java
@@ -0,0 +1,48 @@
+package com.doubledigit.algorithim.chapter1.datastructures.examples;
+
+
+//input: [20, 10, 50, 30, 60, 80, 10], output: 90
+public class MaxProfitFromGivenPrices {
+
+    private static final int[] PRICES = {200, 20, 50, 300, 60, 800, 10};
+//    private static final int[] PRICES = {10, 40, 30, 20, 10, 5, 3};
+
+    public static void main(String[] args) {
+
+        int maxProfit = getMaxProfit(PRICES);
+        System.out.println("maxProfit = " + maxProfit);
+
+    }
+
+    private static int getMaxProfit(int[] prices) {
+        if (prices.length < 2) {
+            return 0;
+        }
+
+        int n = prices.length;
+        int finalProfit = 0;
+
+        int i = 0;
+        while (i < n - 1) {
+
+            while ((i < n - 1) && (prices[i + 1] < prices[i])) {
+                i++;
+            }
+            if (i == n - 1) {
+                break;
+            }
+            int buyPrice = prices[i];
+            i++;
+
+            while ((i < n - 1) && (prices[i + 1] >= prices[i])) {
+                i++;
+            }
+
+            int sellPrice = prices[i];
+            i++;
+
+            finalProfit = (finalProfit + Math.abs(buyPrice - sellPrice));
+        }
+        return finalProfit;
+    }
+}
---
+++ src/main/java/com/doubledigit/algorithim/chapter1/datastructures/examples/MaxSumFromSubsetArrayNoAdjacent.java
@@ -0,0 +1,52 @@
+package com.doubledigit.algorithim.chapter1.datastructures.examples;
+
+
+//inputData: [4, 12, 11, 10, 9, 14, 6], output: [4, 11, 9, 6] : 30
+public class MaxSumFromSubsetArrayNoAdjacent {
+
+    private static final int[] inputData = {14, 12, 2, 13, 9, 1, 0};
+
+    public static void main(String[] args) {
+        int maxSum = maxSumSubsetWithNoAdjacent(inputData);
+        int maxSumSecond = maxSumSubsetWithNoAdjacentEfficient(inputData);
+        System.out.println("maxSum = " + maxSum);
+        System.out.println("maxSumSecond = " + maxSumSecond);
+    }
+
+    private static int maxSumSubsetWithNoAdjacentEfficient(int[] inputData) {
+        if(inputData.length == 0){
+            return 0;
+        } else if(inputData.length == 1){
+            return inputData[0];
+        }
+
+        int first = inputData[0];
+        int second = Math.max(first, inputData[1]);
+
+        for (int i = 2; i< inputData.length; i++){
+            int current = Math.max(second, first + inputData[i]);
+            first = second;
+            second = current;
+        }
+        return second;
+    }
+
+    //time: O(n) | space: O(n)
+    private static int maxSumSubsetWithNoAdjacent(int[] inputData) {
+        if(inputData.length == 0){
+            return 0;
+        } else if(inputData.length == 1){
+            return inputData[0];
+        }
+
+        int[] maxSums = inputData.clone();
+        maxSums[1] = Math.max(inputData[0], inputData[1]);
+        for (int i = 2; i< inputData.length; i++){
+            maxSums[i] = Math.max(maxSums[i-1], maxSums[i-2] + inputData[i]);
+        }
+
+        return maxSums[inputData.length -1];
+    }
+
+
+}
