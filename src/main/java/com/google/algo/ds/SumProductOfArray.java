3df6098dfc16eb413566e18eff8bea9a35e998cc
vivek22117 <vivekmishra22117@gmail.com>
2021-10-13 20:31:33 +0530

    adding new algo

---
+++ src/main/java/com/google/algo/ds/SumProductOfArray.java
@@ -0,0 +1,25 @@
+package com.google.algo.ds;
+
+public class SumProductOfArray {
+
+    public static void main(String[] inputs) {
+        SumProductOfArray sumProductOfArray = new SumProductOfArray();
+        int[] input = {3, 5, 8, 9, 1, 6, 8};
+        sumProductOfArray.sumOfArray(input);
+    }
+
+    void sumOfArray(int[] array) {
+        int sum = 0;
+        int product = 1;
+
+        for(int i : array) {
+            sum += i;
+        }
+
+        for(int i : array) {
+            product *= i;
+        }
+
+        System.out.println(sum + ", " + product);
+    }
+}
