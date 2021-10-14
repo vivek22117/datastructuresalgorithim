655f534404d0acf70380b9b23d6aae74f819b183
vivek22117 <vivekmishra22117@gmail.com>
2021-10-14 22:00:08 +0530

    adding new algo

---
+++ src/main/java/com/google/algo/FibonacciRecursive.java
@@ -0,0 +1,22 @@
+package com.google.algo;
+
+public class FibonacciRecursive {
+
+    public static void main(String[] args) {
+        FibonacciRecursive fibonacciRecursive = new FibonacciRecursive();
+        int output = fibonacciRecursive.findFibonacci(12);
+        System.out.println(output);
+    }
+
+    public int findFibonacci(int num) {
+        if (num < 0) {
+            return -1;
+        }
+
+        if( num == 0 || num == 1) {
+            return 1;
+        }
+
+        return findFibonacci(num -1) + findFibonacci(num -2);
+    }
+}
