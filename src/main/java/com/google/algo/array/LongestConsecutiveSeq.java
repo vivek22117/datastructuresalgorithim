260b1f639c62768aade8c01f1afd4db5976b8f5d
vivek22117 <vivekmishra22117@gmail.com>
2021-10-16 21:31:22 +0530

    adding new algo

--- src/main/java/com/google/algo/array/LongestConsecutiveSeq.java
+++ src/main/java/com/google/algo/array/LongestConsecutiveSeq.java
@@ -1,15 +1,47 @@
 package com.google.algo.array;
 
+import java.util.HashSet;
+
 public class LongestConsecutiveSeq {
 
-    private static int[] input = {4, 200, 34, 1, 10, 300, 2, 33, 3};
+    private static int[] input = {4, 200, 34, 1, 0, 300, 2, 33, 3};
 
     public static void main(String[] args) {
         LongestConsecutiveSeq sequence = new LongestConsecutiveSeq();
         int length = sequence.longestConsecutive(input);
         System.out.println(length);
+
+        int length2 = sequence.longestConsecutiveWithExtraSpace(input);
+        System.out.println(length2);
     }
 
+    public int longestConsecutiveWithExtraSpace(int[] nums) {
+        HashSet<Integer> numSet = new HashSet<>();
+
+        for(int i : nums) {
+            numSet.add(i);
+        }
+
+        int max_sequence = 0;
+
+        for(int i = 0; i < nums.length; i++) {
+            int current_num = nums[i];
+            int current_seq_len = 1;
+
+            if(!numSet.contains(current_num - 1)) {
+                while(numSet.contains(current_num + 1)) {
+                    current_num += 1;
+                    current_seq_len += 1;
+                }
+            }
+            max_sequence = Math.max(max_sequence,  current_seq_len);
+
+        }
+
+        return max_sequence;
+    }
+
+//  This solution is O(n2) OR O(n3)
     public int longestConsecutive(int[] input) {
         int max_sequence = 0;
 
