8aaea3da798ef4bb92502d036268f9e52e766e22
vivek22117 <vivekmishra22117@gmail.com>
2021-11-19 13:16:33 +0530

    adding new algo

--- src/main/java/com/google/algo/array/ShortestWaySubString.java
+++ src/main/java/com/google/algo/array/ShortestWaySubString.java
@@ -11,5 +11,24 @@ Output: 3  How: "ac" + "b" + "ac"
 
 public class ShortestWaySubString {
 
+    private static String sourceString = "abc";
+    private static String targetStrng = "acbac";
+
+    public static void main(String[] args) {
+
+    }
+
+
+    public int findNumberOfSubSequences(String src, String target) {
+        int numOfSequences = 0;
+
+        while (target.length() > 0) {
+            int srcIndex = 0;
+            int targetIndex = 0;
+            while (srcIndex < src.charAt(srcIndex) && targetIndex < target.charAt(targetIndex)) {
+
+            }
+        }
+    }
 
 }
