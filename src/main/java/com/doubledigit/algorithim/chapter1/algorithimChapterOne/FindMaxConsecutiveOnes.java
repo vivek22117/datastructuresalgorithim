1c4ad64e00b492b74df23850f4c0888eb139c477
vivek22117 <vivekmishra22117@gmail.com>
2021-11-29 13:33:40 +0530

    adding index logic in the consecutive ones

--- src/main/java/com/doubledigit/algorithim/chapter1/algorithimChapterOne/FindMaxConsecutiveOnes.java
+++ src/main/java/com/doubledigit/algorithim/chapter1/algorithimChapterOne/FindMaxConsecutiveOnes.java
@@ -10,6 +10,11 @@ public class FindMaxConsecutiveOnes {
 //    private static int[] inputData = {1, 0, 1};
 
     public static void main(String[] args) {
+
+        FindMaxConsecutiveOnes ones = new FindMaxConsecutiveOnes();
+        String maxConsecutiveOnesIndex = ones.findMaxConsecutiveOnesIndex(inputData);
+        System.out.println(maxConsecutiveOnesIndex);
+
         int N = inputData.length;
         int i = 0;
         int max_length = Integer.MIN_VALUE;
@@ -28,5 +33,32 @@ public class FindMaxConsecutiveOnes {
             }
         }
         System.out.println("Maximum number of One's are: " + max_length);
+    }
+
+    public String findMaxConsecutiveOnesIndex(int[] nums) {
+        int N = nums.length;
+        int maxLength = Integer.MIN_VALUE;
+        String indexes = "";
+
+        for(int i = 0; i < N; i++) {
+            if(nums[i] == 1) {
+                int r = i + 1;
+                int count = 1;
+
+                while (r < N) {
+                    if(nums[r] != 1) break;
+                    count = count + 1;
+                    r++;
+                }
+
+                if(maxLength < count) {
+                    maxLength = count;
+                    indexes = i + " to " + (r - 1);
+                }
+                i = r;
+            }
+        }
+        System.out.println(maxLength);
+        return indexes;
     }
 }
